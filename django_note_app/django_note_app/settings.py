7ff94e15042a4f635d1f6a29cae01b35d2c6d738
Bartekdevv <bartlomiejbogdan.dev@gmail.com>
2023-02-22 17:35:59 +0100

    enable all hosts

--- django_note_app/django_note_app/settings.py
+++ django_note_app/django_note_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u1252xjfh+3gjhz#oq@c7z_ixfd^(zawx!6d!5xa3s7ep-5_rz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
