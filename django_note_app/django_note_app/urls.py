e5e9201bb6607d790d21d9fdf54565d6e9e48e8f
Bartekdevv <bartlomiejbogdan.dev@gmail.com>
2023-02-23 20:54:40 +0100

    add black package

--- django_note_app/django_note_app/urls.py
+++ django_note_app/django_note_app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('django_note_app_frontend.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("django_note_app_frontend.urls")),
 ]
