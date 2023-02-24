8aa704cbba871886f909b912567389d3c473582b
Bartekdevv <bartlomiejbogdan.szarlej@gmail.com>
2023-02-24 11:58:08 +0100

    add template context

--- django_note_app/django_note_app_frontend/views.py
+++ django_note_app/django_note_app_frontend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django_note_app_frontend.models import Field
+from django_note_app_frontend.forms import NoteForm
 
 
 def home(request):
-    return render(request, "home.html")
+    template_name = "home.html"
+    context = {
+        "Fields": Field.objects.all(),
+        "NoteForm": NoteForm(),
+    }
+    return render(request, "home.html", context)
