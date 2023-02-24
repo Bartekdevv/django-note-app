3f76cd0d2951572ec321bb1aa059a090bb037927
Bartekdevv <bartlomiejbogdan.szarlej@gmail.com>
2023-02-24 11:58:26 +0100

    add new Models Note and Field

--- django_note_app/django_note_app_frontend/admin.py
+++ django_note_app/django_note_app_frontend/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django_note_app_frontend.models import Note, Field
 
-# Register your models here.
+admin.site.register(Note)
+admin.site.register(Field)
--- django_note_app/django_note_app_frontend/models.py
+++ django_note_app/django_note_app_frontend/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Field(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    description = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.description
+
+
+class Note(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    field = models.ForeignKey(Field, on_delete=models.CASCADE, default=Field.objects.get(description="General"))
+    text = models.TextField()
+    created_at = models.DateField(default=now)
